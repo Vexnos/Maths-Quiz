8b085b24b0ccac1a9d4ae642573d0c841952599e
Mathmagician8191 <50558333+Mathmagician8191@users.noreply.github.com>
2021-11-22 13:49:48 +1300

    Made program harder to quit

--- speedrunning.py
+++ speedrunning.py
@@ -25,8 +25,8 @@
 --------------------------------------------------------------------
 '''
 #-------Libraries-------
-import random #Imports the random library which allows the program to generate random numbers for the question
-import time #Imports the time library which allows the program to slow down the interval between actions and allows the quiz to be timed
+from random import randint #Imports the random library which allows the program to generate random numbers for the question
+from time import time #Imports the time library which allows the program to slow down the interval between actions and allows the quiz to be timed
 
 #-------Functions-------
 
@@ -45,19 +45,16 @@ def diff():
     else:
       print("Invalid difficulty entered, please try again")
 
-#This is the guess validator, it ensures the user cannot enter an invalid value as their answer to the question (This function is designed to work within the questions function)
-def guess_validator(question):
-  while True: #Loops to ensure that an invalid value cannot be stored
+#This is the answer checker, it checks if the user's guess is the same as the answer, if it is, the user gains a point, if not, a point is revoked (This function is designed to work within the question function)
+def answer_checker(question,answer,score):
+  keep_going = True
+  while keep_going: #Loops to ensure that an invalid value cannot be stored
     try:
       guess = int(input("Please type the answer here: "))
-      return guess
+      keep_going = False
     except ValueError:
       print("Invalid value entered, please enter a number and try again")
       print(question) #Prints the question again so the user doesn't have to scroll up
-
-#This is the answer checker, it checks if the user's guess is the same as the answer, if it is, the user gains a point, if not, a point is revoked (This function is designed to work within the question function)
-def answer_checker(question,answer,score):
-  guess = guess_validator(question) #Calls the guess_validator function to ensure an invalid value is not treated as an incorrect answer
 
   if guess == answer:
     score += 1 #Adds 1 point to the score
@@ -68,13 +65,12 @@ def answer_checker(question,answer,score):
 
 #This is the question generator, it will generate 10 random questions using the factors from the diff() function.
 def questions(n1,n2):
-  global score
   QUESTION_AMOUNT = 10 #Sets the amount of questions to generate
   score = 0 #Sets the score to 0 every time the program is activated.
-  start = time.time() #Starts the timer and stores the recorded time in a variable
-  for i in range(QUESTION_AMOUNT): #Loops the question generator 10 times
-    num1 = random.randint(n1,n2) #Generates a random number using the factors from the diff() function
-    num2 = random.randint(n1,n2)
+  start = time() #Starts the timer and stores the recorded time in a variable
+  for _ in range(QUESTION_AMOUNT): #Loops the question generator 10 times
+    num1 = randint(n1,n2) #Generates a random number using the factors from the diff() function
+    num2 = randint(n1,n2)
     
     answer = num1 * num2 #Defines the answer for each question
     question = f"\n{num1} x {num2}\n" #Sets the question using the randomly generated numbers
@@ -83,8 +79,7 @@ def questions(n1,n2):
 
     score = answer_checker(question,answer,score) #Calls the answer_checker function
 
-  stop = time.time() #Stops the timer and stores the recorded time in a variable
-  user_time = stop-start #Subtracting the start time from stop time allows us to find the user's time.
+  user_time = time()-start #Subtracting the start time from stop time allows us to find the user's time.
 
   print(f"You got {score}/10 questions right in a time of {user_time:.2f}s") #{user_time:.2f} Displays the user's time to 2 significant figures
 
@@ -95,6 +90,6 @@ if __name__ == "__main__":
     questions(n1,n2) #Calls the questions function
 
     play_again = input("Play again? (y/n): ") #Asks if the user wants to play again and stores their choice in a variable
-    if play_again.lower() != "y":
+    if not play_again.lower().startswith("y"):
       print("Thanks for playing, goodbye!\n")
       quit()
